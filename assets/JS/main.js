73dd0e3f2b8e3a7ad649cce62de1a09bb1f85a6d
Eric Dangerstone <Eric@Dangerstone.com>
2019-04-27 23:46:01 -0500

    Recreated semi circle nav bar, implemented svg

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,84 +1,40 @@
 function(){
+	var svg = document.getElementById('menu'),
+	    items = svg.querySelectorAll('.item'),
+	    trigger = svg.getElementById('trigger'),
+	    label = trigger.querySelectorAll('#label')[0],
+	    open = false;
 
-	var abutton = document.getElementById('about-button'),
-    awrapper = document.getElementById('about-wrapper');
-	var dbutton = document.getElementById('donate-button'),
-    dwrapper = document.getElementById('donate-wrapper');
-	var	clbutton = document.getElementById('class-button'),
-	  clwrapper = document.getElementById('class-wrapper');
-	var	cabutton = document.getElementById('camp-button'),
-		cawrapper = document.getElementById('camp-wrapper');
-	var	pbutton = document.getElementById('prod-button'),
-		pwrapper = document.getElementById('prod-wrapper');
-    //open and close menu when the button is clicked
-	var aopen = false;
-	var dopen = false;
-	var clopen = false;
-	var caopen = false;
-	var popen = false;
-	button.addEventListener('click', ahandler, false);
-	button.addEventListener('click', dhandler, false);
-	button.addEventListener('click', clhandler, false);
-	button.addEventListener('click', cahandler, false);
-	button.addEventListener('click', phandler, false);
+	    //first scale the elements down
+	    TweenLite.set(items, {scale:0, visibility:"visible"});
+	svg.style.pointerEvents = "none";
 
-	function ahandler(){
-	  if(!open){
-	    this.innerHTML = "Close";
-	    classie.add(wrapper, 'opened-nav');
-	  }
-	  else{
-	    this.innerHTML = "Menu";
-		classie.remove(wrapper, 'opened-nav');
-	  }
-	  open = !open;
+	    //set up event handler
+	    trigger.addEventListener('click', toggleMenu, false);
+
+	    //toggle menu when trigger is clicked
+	    function toggleMenu(event) {
+	     if (!event) var event = window.event;
+	    event.stopPropagation();
+	    open = !open;
+	    if (open) {
+	        TweenMax.staggerTo(items, 0.7, {scale:1, ease:Elastic.easeOut}, 0.05);
+	        label.innerHTML = "-";
+	      svg.style.pointerEvents = "auto";
+	    } else {
+	        TweenMax.staggerTo(items, .3, {scale:0, ease:Back.easeIn}, 0.05);
+	      svg.style.pointerEvents = "none";
+	    }
+	    }
+
+	svg.onclick = function (e) {
+	    e.stopPropagation();
 	}
-	function dhandler(){
-	  if(!open){
-	    this.innerHTML = "Close";
-	    classie.add(wrapper, 'opened-nav');
-	  }
-	  else{
-	    this.innerHTML = "Menu";
-		classie.remove(wrapper, 'opened-nav');
-	  }
-	  open = !open;
-	}
-	function clhandler(){
-	  if(!open){
-	    this.innerHTML = "Close";
-	    classie.add(wrapper, 'opened-nav');
-	  }
-	  else{
-	    this.innerHTML = "Menu";
-		classie.remove(wrapper, 'opened-nav');
-	  }
-	  open = !open;
-	}
-	function cahandler(){
-	  if(!open){
-	    this.innerHTML = "Close";
-	    classie.add(wrapper, 'opened-nav');
-	  }
-	  else{
-	    this.innerHTML = "Menu";
-		classie.remove(wrapper, 'opened-nav');
-	  }
-	  open = !open;
-	}
-	function phandler(){
-	  if(!open){
-	    this.innerHTML = "Close";
-	    classie.add(wrapper, 'opened-nav');
-	  }
-	  else{
-	    this.innerHTML = "Menu";
-		classie.remove(wrapper, 'opened-nav');
-	  }
-	  open = !open;
-	}
-	function closeWrapper(){
-		classie.remove(wrapper, 'opened-nav');
-	}
+	//close the nav when document is clicked
+	document.onclick = function () {
+	    open = false;
+	    TweenMax.staggerTo(items, .3, {scale:0, ease:Back.easeIn}, 0.05);
+	    svg.style.pointerEvents = "none";
+	};
 
 })();
