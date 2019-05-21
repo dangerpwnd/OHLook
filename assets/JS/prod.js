0e3a331431cf973d6e7f78aa170d5247b1d80fc7
Eric Martin <Eric.M@cathedralplumbingtx.com>
2019-05-21 15:51:49 -0500

    Fixed flip card animation in productions, added example prod images

--- assets/JS/prod.js
+++ assets/JS/prod.js
@@ -1,6 +1,6 @@
-var card = document.getElementsByClassName("card");
-for (var i=0;i<card.length;i++){
-        card[i].addEventListener('click', function() {
-      card[i].classList.toggle('is-flipped');
-    }, false);
-  };
+$(document).ready(
+    function () {
+        $('.card').click(function () {
+            $(this).toggleClass('is-flipped');
+        });
+    });
