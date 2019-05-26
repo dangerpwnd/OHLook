4f53a045d1f593a00d08fb0c3ad0b6e5f2a39e97
Eric Dangerstone <Eric@Dangerstone.com>
2019-05-25 21:36:04 -0500

    Additional work on nav bar, added content to auditions page, updated grid layout

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,32 +1,68 @@
-var tl = new TimelineMax({paused: false});
+$(document).ready(function(){
+	var tl = new TimelineMax({paused: false});
 
-tl.to(".one", 0.8, {
-	y: 6,
-	rotation: 45,
-	ease: Expo.easeInOut
+	tl.add(TweenMax.to(".one", 0.5, {
+		y: 6,
+		rotation: 45,
+		ease: Expo.easeInOut
+	}));
+
+	tl.add(TweenMax.to(".two", 0.5, {
+		y:-6,
+		rotation: -45,
+		ease: Expo.easeInOut,
+		delay: -0.5
+	}));
+
+	tl.add(TweenMax.to(".menu", 1, {
+		top: "0%",
+		ease: Expo.easeInOut,
+		delay: -1
+	}));
+
+	tl.add(TweenMax.staggerFrom(".navtop", 1, {
+		x: -200,
+		opacity: 0,
+		ease: Expo.easeOut
+	}, 0.2));
+	
+	tl.reverse();
+
+	$(".toggle-btn").click(function(){
+		tl.reversed(!tl.reversed());
+	});
+
 });
 
-tl.to(".two", 0.8, {
-	y:-6,
-	rotation: -45,
-	ease: Expo.easeInOut,
-	delay: -0.8
-});
-
-tl.to(".menu", 2, {
-	top: "0%",
-	ease: Expo.easeInOut,
-	delay: -2
-});
-
-tl.staggerFrom("menu ul li", 2, {
-	x: -200,
+$(".navTopP").click(function() {
+	if($(".navSubP").style.display === none){
+	TweenMax.staggerFrom($(".navSubP"), 1, {
+	display: "block",
 	opacity: 0,
 	ease: Expo.easeOut
-}, 0.3);
+	})}
+	else{
+		$(".navSubP").style.display === none;
+	}
+	});
 
-tl.reverse();
+$(".navTopCl").click(function() {
+	TweenMax.staggerFrom($(".navSubCl"), 1, {
+	display: "block",
+	opacity: 0,
+	ease: Expo.easeOut
+	})});
 
-$(".toggle-btn").click(function(){
-	tl.reversed(!tl.reversed());
-});
+$(".navTopCa").click(function() {
+	TweenMax.staggerFrom($(".navSubCa"), 1, {
+	display: "block",
+	opacity: 0,
+	ease: Expo.easeOut
+	})});
+
+$(".navTopD").click(function() {
+	TweenMax.staggerFrom($(".navSubD"), 1, {
+	display: "block",
+	opacity: 0,
+	ease: Expo.easeOut
+	})});
