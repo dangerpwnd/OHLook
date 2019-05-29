4e09030dd3a3aa15223cc78433f4b912a8edeec8
Eric Dangerstone <Eric@Dangerstone.com>
2019-05-28 23:22:51 -0500

    Started new index with more focus on intuitive design, less on visual appeal. Need to install and add h1 phrase to svg hero

---
+++ assets/JS/nav.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	var tl1 = new TimelineMax({paused: false});
+
+	tl1.add(TweenMax.to(".one", 0.5, {
+		y: 6,
+		rotation: 45,
+		ease: Expo.easeInOut
+	}));
+
+	tl1.add(TweenMax.to(".two", 0.5, {
+		y:-6,
+		rotation: -45,
+		ease: Expo.easeInOut,
+		delay: -0.5
+	}));
+
+	tl1.add(TweenMax.to(".menu", 1, {
+		top: "0%",
+		ease: Expo.easeInOut,
+		delay: -1
+	}));
+
+	tl1.add(TweenMax.staggerFrom(".navtop", 1, {
+		x: -200,
+		opacity: 0,
+		ease: Expo.easeOut
+	}, 0.2));
+	
+	tl1.reverse();
+
+	$(".toggle-btn").click(function(){
+		tl1.reversed(!tl1.reversed());
+	});
+
+var tl2 = new TimelineMax({paused: false});
+
+	tl2.add($(".navTopP").hover(function() {
+		TweenMax.staggerFrom($(".navSubP"), 1, {
+		display: "block",
+		opacity: 0,
+		ease: Expo.easeOut
+	})}));
+	
+	tl2.add($(".navTopCl").hover(function() {
+		TweenMax.staggerFrom($(".navSubCl"), 1, {
+		display: "block",
+		opacity: 0,
+		ease: Expo.easeOut
+	})}));
+
+	tl2.add($(".navTopCa").hover(function() {
+	TweenMax.staggerFrom($(".navSubCa"), 1, {
+	display: "block",
+	opacity: 0,
+	ease: Expo.easeOut
+	})}));
+
+	tl2.add($(".navTopD").hover(function() {
+	TweenMax.staggerFrom($(".navSubD"), 1, {
+	display: "block",
+	opacity: 0,
+	ease: Expo.easeOut
+	})}));
+
+	tl2.reverse();
+
+});
