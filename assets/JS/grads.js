19ee2046f60705c734be7fb0270d652a30fcba08
Eric Dangerstone <Eric@Dangerstone.com>
2019-06-09 19:36:03 -0500

    Added tween animation to grad pics. Updated grad list

---
+++ assets/JS/grads.js
@@ -0,0 +1,55 @@
+var expandLeftGrad = document.querySelectorAll(".leftTile");
+var expandRightGrad = document.querySelectorAll(".rightTile");
+var expandMidGrad = document.querySelectorAll(".midTile");
+
+expandLeftGrad.forEach(function(grad){
+    var timeLine = new TimelineMax();
+    var tween = timeLine.add(TweenMax.to(grad,1, {xPercent: 100, autoAlpha: 1, zIndex:5, scale:2.5, ease: Power2.easeOut}));
+    var tween2 = timeLine.add(TweenMax.to(grad.querySelectorAll('.gradInfo'), 1, {yPercent: -100, autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+    grad.addEventListener("click", toggleAnimation);
+    timeLine.reverse();
+    function toggleAnimation(){
+        if(timeLine.reversed()){
+            timeLine.play();
+        }
+        else{
+            timeLine.reverse();
+        }
+    }
+});
+
+expandRightGrad.forEach(function(grad){
+    var timeLine = new TimelineMax();
+    var tween = timeLine.add(TweenMax.to(grad,1, {xPercent: -100, autoAlpha: 1, zIndex:5, scale:2.8, ease: Power2.easeOut}));
+    var tween2 = timeLine.add(TweenMax.to(grad.querySelectorAll('.gradInfo'), 1, {yPercent: -100, autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+    grad.addEventListener("click", toggleAnimation);
+    timeLine.reverse();
+    function toggleAnimation(){
+        if(timeLine.reversed()){
+            timeLine.play();
+        }
+        else{
+            timeLine.reverse();
+        }
+    }
+});
+
+expandMidGrad.forEach(function(grad){
+    var timeLine = new TimelineMax();
+    var tween = timeLine.add(TweenMax.to(grad,1, {yPercent: -100, autoAlpha: 1, zIndex:5, scale:2.8, ease: Power2.easeOut}));
+    var tween2 = timeLine.add(TweenMax.to(grad.querySelectorAll('.gradInfo'), 1, {yPercent: -100, autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+    grad.addEventListener("click", toggleAnimation);
+    timeLine.reverse();
+    function toggleAnimation(){
+        if(timeLine.reversed()){
+            timeLine.play();
+        }
+        else{
+            timeLine.reverse();
+        }
+    }
+});
+
+    
+
+
