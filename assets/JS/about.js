dc1c2d037467130ce7cb53d84db32044413d62f6
Eric Martin <Eric.M@cathedralplumbingtx.com>
2019-06-12 16:54:54 -0500

    Added animations to about page

--- assets/JS/about.js
+++ assets/JS/about.js
@@ -1,12 +1,23 @@
-const panels = document.querySelectorAll('.aboutInfo');
-	function toggleOpen() {
-		this.classList.toggle('open');
-	}
-	function toggleActive(e) {
-		console.log(e.propertyName);
-		if (e.propertyName.includes('flex')) {
-			this.classList.toggle('open-active');
-		}
-	}
-	panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-	panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+var panels = document.querySelectorAll('.aboutInfo');
+
+
+panels.forEach(function(panel){
+    var timeLine = new TimelineMax();
+	var tween = timeLine.add(TweenMax.to(panel,1, {flex:6}));
+	var tween2 = timeLine.add(TweenMax.to(panel.querySelector("h1"), 0.5, {autoAlpha:0, ease: Power2.easeOut}));
+	var tween3 = timeLine.add(TweenMax.to(panel.querySelector("h1"), 0.5, {writingMode:"horizontal-tb", textOrientation:"sideways",autoAlpha:1, ease: Power2.easeOut}));
+	var tween4 = timeLine.add(TweenMax.to(panel.querySelectorAll("h2"), 0.5, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+	var tween5 = timeLine.add(TweenMax.to(panel.querySelectorAll("p"), 0.5, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+	var tween5 = timeLine.add(TweenMax.to(panel.querySelectorAll("li"), 0.5, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+
+    panel.addEventListener("click", toggleAnimation);
+    timeLine.reverse();
+    function toggleAnimation(){
+        if(timeLine.reversed()){
+            timeLine.play();
+        }
+        else{
+            timeLine.reverse();
+        }
+    }
+});
