391807e68183469881fe37062b6fdbc65a04cdb9
Eric Dangerstone <Eric@Dangerstone.com>
2019-06-12 22:57:56 -0500

    Added content to about

--- assets/JS/about.js
+++ assets/JS/about.js
@@ -3,7 +3,7 @@ var panels = document.querySelectorAll('.aboutInfo');
 
 panels.forEach(function(panel){
     var timeLine = new TimelineMax();
-	var tween = timeLine.add(TweenMax.to(panel,1, {flex:6}));
+	var tween = timeLine.add(TweenMax.to(panel,0.5, {flex:6}));
 	var tween2 = timeLine.add(TweenMax.to(panel.querySelector("h1"), 0.5, {autoAlpha:0, ease: Power2.easeOut}));
 	var tween3 = timeLine.add(TweenMax.to(panel.querySelector("h1"), 0.5, {writingMode:"horizontal-tb", textOrientation:"sideways",autoAlpha:1, ease: Power2.easeOut}));
 	var tween4 = timeLine.add(TweenMax.to(panel.querySelectorAll("h2"), 0.5, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
