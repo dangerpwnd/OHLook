deeeb9a408118e58b7871f01b7ab4451dc1aaed5
Eric Dangerstone <Eric@Dangerstone.com>
2019-06-13 18:53:14 -0500

    Added more info, need to add grad pic animation

--- assets/JS/about.js
+++ assets/JS/about.js
@@ -3,12 +3,12 @@ var panels = document.querySelectorAll('.aboutInfo');
 
 panels.forEach(function(panel){
     var timeLine = new TimelineMax();
-	var tween = timeLine.add(TweenMax.to(panel,0.5, {flex:6}));
+	var tween = timeLine.add(TweenMax.to(panel,0.5, {flex:3}));
 	var tween2 = timeLine.add(TweenMax.to(panel.querySelector("h1"), 0.5, {autoAlpha:0, ease: Power2.easeOut}));
 	var tween3 = timeLine.add(TweenMax.to(panel.querySelector("h1"), 0.5, {writingMode:"horizontal-tb", textOrientation:"sideways",autoAlpha:1, ease: Power2.easeOut}));
 	var tween4 = timeLine.add(TweenMax.to(panel.querySelectorAll("h2"), 0.5, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
 	var tween5 = timeLine.add(TweenMax.to(panel.querySelectorAll("p"), 0.5, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
-	var tween5 = timeLine.add(TweenMax.to(panel.querySelectorAll("li"), 0.5, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+	var tween5 = timeLine.add(TweenMax.to(panel.querySelectorAll("ul"), 0.5, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
 
     panel.addEventListener("click", toggleAnimation);
     timeLine.reverse();
