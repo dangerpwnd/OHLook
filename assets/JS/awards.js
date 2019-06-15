6023137f87593d757546b5930b49a1f6abd919a5
Eric Dangerstone <Eric@Dangerstone.com>
2019-06-15 12:43:25 -0500

    Created purchase page, added links on class

--- assets/JS/awards.js
+++ assets/JS/awards.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
         duration: '40%'
     })
     .setClassToggle(this, 'fade-in') //add class to firstpic
-    .addIndicators({
-        name: 'fade scene',
-        colorTrigger: 'black',
-        colorStart: '#75C695',
-        colorEnd: 'pink'
-    }) //requires a plugin
+    // .addIndicators({
+    //     name: 'fade scene',
+    //     colorTrigger: 'black',
+    //     colorStart: '#75C695',
+    //     colorEnd: 'pink'
+    // }) //requires a plugin
     .addTo(controller);
     })
 
