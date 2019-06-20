ad2c220d52c0f05fac25cbf8a0e8a3c9e8c39d3a
Eric Martin <Eric.M@cathedralplumbingtx.com>
2019-06-20 12:37:52 -0500

    Created tile structure for about page, updated css/js

--- assets/JS/about.js
+++ assets/JS/about.js
@@ -4,8 +4,8 @@ var expandBio = document.querySelectorAll('.bio');
 //bio tile animation
 expandBio.forEach(function(bio){
     var timeLine = new TimelineMax();
-    var tween = timeLine.add(TweenMax.to(bio,1, {xPercent: 100, autoAlpha: 1, zIndex:5, scale:2.5, ease: Power2.easeOut}));
-    var tween2 = timeLine.add(TweenMax.to(bio.querySelectorAll('.bioInfo'), 1, {yPercent: -100, autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+    var tween = timeLine.add(TweenMax.to(bio,1, {autoAlpha: 1, zIndex:10, scale:1.8, ease: Power2.easeOut}));
+    var tween2 = timeLine.add(TweenMax.to(bio.querySelectorAll('.bioInfo'), 1, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
     bio.addEventListener("click", toggleAnimation);
     timeLine.reverse();
     function toggleAnimation(){
