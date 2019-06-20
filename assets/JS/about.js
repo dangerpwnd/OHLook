5daf0593068667d8838d15e5bf5db31d6b33c39f
Eric Martin <Eric.M@cathedralplumbingtx.com>
2019-06-20 12:59:35 -0500

    Added trans background to info

--- assets/JS/about.js
+++ assets/JS/about.js
@@ -5,7 +5,8 @@ var expandBio = document.querySelectorAll('.bio');
 expandBio.forEach(function(bio){
     var timeLine = new TimelineMax();
     var tween = timeLine.add(TweenMax.to(bio,1, {autoAlpha: 1, zIndex:10, scale:1.8, ease: Power2.easeOut}));
-    var tween2 = timeLine.add(TweenMax.to(bio.querySelectorAll('.bioInfo'), 1, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+    var tween2 = timeLine.add(TweenMax.to(bio.querySelectorAll('.transBack'), 0.3, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
+    var tween3 = timeLine.add(TweenMax.to(bio.querySelectorAll('.bioInfo'), 1, {autoAlpha:1, visibility:'visible', ease: Power2.easeOut}));
     bio.addEventListener("click", toggleAnimation);
     timeLine.reverse();
     function toggleAnimation(){
