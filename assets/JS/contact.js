6194295ad742c80bef09f8b8ce3409d401f41594
Eric Martin <Eric.M@cathedralplumbingtx.com>
2019-06-27 14:20:24 -0500

    Added recaptcha and validation

---
+++ assets/JS/contact.js
@@ -0,0 +1,27 @@
+$(".contactForm").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    var message = $("#message").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "assets/php/send.php",
+        data: "name=" + name + "&email=" + email + "&phone=" + phone + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $( "#success" ).removeClass( "hidden" );
+}
--- assets/php/send.php
+++ assets/php/send.php
@@ -16,21 +16,16 @@ if(isset($_POST['submit'])){
 
     $headers = 'From: '.$email."\r\n";
     $headers .= 'Reply-To: '.$email."\r\n";
-
+    
+    //send email
     $mail_status = mail($mail_to, $subject, $body, $headers);
+
+    //redirect to success page
+    if($mail_status){
+        echo "success";
+    }
+    else{
+        echo "invalid";
+    }
 }
-//     if ($mail_status) { 
-// 	<script language="javascript" type="text/javascript">
-// 		alert('Thank you for the message. We will contact you shortly.');
-// 		window.location = 'contact.html';
-// 	</script>
-// <?php
-// }
-// else { 
-// 	<script language="javascript" type="text/javascript">
-// 		alert('Message failed. Please, send an email to jlord@ohlookcreative.com');
-// 		window.location = 'contact.html';
-// 	</script>
-// <?php
-// }
 ?>
