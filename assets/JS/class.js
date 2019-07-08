ab8521d930a36a597fbbbc9115a4e0057d00077b
Eric Martin <Eric.M@cathedralplumbingtx.com>
2019-07-08 13:27:00 -0500

    Updated links and event listener to look for hover vs click

--- assets/JS/class.js
+++ assets/JS/class.js
@@ -4,10 +4,21 @@
 		this.classList.toggle('open');
 	}
 	function toggleActive(e) {
-		console.log(e.propertyName);
 		if (e.propertyName.includes('flex')) {
 			this.classList.toggle('open-active');
 		}
 	}
-	panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-	panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+	// panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+	// Hover over item to expand and add open class
+	panels.forEach(panel => panel.addEventListener('focus', toggleOpen));
+	panels.forEach(panel => panel.addEventListener('mouseenter', toggleOpen));
+	panels.forEach(panel => panel.addEventListener('mouseleave', toggleOpen));
+	
+	//Will toggle open-active on flexed items at end of transition
+	panels.forEach(function(panel){
+		if(!panel.className.includes('toggleActive')){
+			panel.addEventListener('transitionend', toggleActive);}
+	});
