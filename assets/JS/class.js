ca274b5e70de5fbd40a6e17b970eadc7993e721d
Eric Dangerstone <reric2012@gmail.com>
2019-07-16 13:47:37 -0500

    Added functionality for mobile screens

--- assets/JS/class.js
+++ assets/JS/class.js
@@ -2,6 +2,7 @@
 const panels = document.querySelectorAll(".panel");
 const titles = document.querySelectorAll(".panelTitle");
 const links = document.querySelectorAll(".panelLink");
+var width = $(window).width();
 
 TweenLite.set(titles, {
 	yPercent: 50
@@ -11,14 +12,14 @@ TweenLite.set(links, {
 	xPercent: 100
 })
 
-panels.forEach(function(panel){
+if(width <= 1024){
+	panels.forEach(function(panel){
 
 	var timeLine = new TimelineMax();
 	var tween = timeLine.add(TweenMax.to(panel, 0.25, {flex: 3, filter: "greyscale(0)"}));
 	var tween2 = timeLine.add(TweenMax.to(panel.querySelectorAll(".panelTitle"),0.25, {yPercent: 0, ease: Power2.easeOut}));
 	var tween3 = timeLine.add(TweenMax.to(panel.querySelectorAll(".panelLink"), 0.25, {xPercent: 0, autoAlpha:1, visibility: "visible", ease: Power2.easeOut}))
-	panel.addEventListener("mouseenter", toggleAnimation);
-	panel.addEventListener("mouseleave", toggleAnimation);
+	panel.addEventListener("click", toggleAnimation);
     timeLine.reverse();
     function toggleAnimation(){
         if(timeLine.reversed()){
@@ -29,3 +30,24 @@ panels.forEach(function(panel){
         }
 	}
 })
+}
+else{
+	panels.forEach(function(panel){
+
+		var timeLine = new TimelineMax();
+		var tween = timeLine.add(TweenMax.to(panel, 0.25, {flex: 3, filter: "greyscale(0)"}));
+		var tween2 = timeLine.add(TweenMax.to(panel.querySelectorAll(".panelTitle"),0.25, {yPercent: 0, ease: Power2.easeOut}));
+		var tween3 = timeLine.add(TweenMax.to(panel.querySelectorAll(".panelLink"), 0.25, {xPercent: 0, autoAlpha:1, visibility: "visible", ease: Power2.easeOut}))
+		panel.addEventListener("mouseenter", toggleAnimation);
+		panel.addEventListener("mouseleave", toggleAnimation);
+		timeLine.reverse();
+		function toggleAnimation(){
+			if(timeLine.reversed()){
+				timeLine.play();
+			}
+			else{
+				timeLine.reverse();
+			}
+		}
+	})
+}
--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,5 +1,7 @@
 	//Animation for index
 	const panels = document.querySelectorAll('.panel');
+	var width = $(window).width();
+
 	function toggleOpen() {
 		this.classList.toggle('open');
 	}
@@ -9,16 +11,22 @@
 		}
 	}
 
-
-	// panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-
-	// Hover over item to expand and add open class
-	panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-	panels.forEach(panel => panel.addEventListener('mouseenter', toggleOpen));
-	panels.forEach(panel => panel.addEventListener('mouseleave', toggleOpen));
-	
-	//Will toggle open-active on flexed items at end of transition
-	panels.forEach(function(panel){
+	if(width <= 1024){
+		// Hover over item to expand and add open class
+		panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+		//Will toggle open-active on flexed items at end of transition
+		panels.forEach(function(panel){
 		if(!panel.className.includes('toggleActive')){
-			panel.addEventListener('transitionend', toggleActive);}
-	});
+		panel.addEventListener('transitionend', toggleActive);}})
+	}
+	else{
+		panels.forEach(panel => panel.addEventListener('mouseenter', toggleOpen));
+		panels.forEach(panel => panel.addEventListener('mouseleave', toggleOpen));
+	
+		//Will toggle open-active on flexed items at end of transition
+		panels.forEach(function(panel){
+		if(!panel.className.includes('toggleActive')){
+			panel.addEventListener('transitionend', toggleActive);}})
+	}
+	
+	
