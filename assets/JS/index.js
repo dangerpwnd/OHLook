48973b1cd854a2d5e8e3b0679f15b84108aa8157
Eric Dangerstone <reric2012@gmail.com>
2019-07-16 12:08:51 -0500

    Created javascript for flex animation

---
+++ assets/JS/index.js
@@ -0,0 +1,24 @@
+	//Animation for index
+	const panels = document.querySelectorAll('.panel');
+	function toggleOpen() {
+		this.classList.toggle('open');
+	}
+	function toggleActive(e) {
+		if (e.propertyName.includes('flex')) {
+			this.classList.toggle('open-active');
+		}
+	}
+
+
+	// panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+	// Hover over item to expand and add open class
+	panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+	panels.forEach(panel => panel.addEventListener('mouseenter', toggleOpen));
+	panels.forEach(panel => panel.addEventListener('mouseleave', toggleOpen));
+	
+	//Will toggle open-active on flexed items at end of transition
+	panels.forEach(function(panel){
+		if(!panel.className.includes('toggleActive')){
+			panel.addEventListener('transitionend', toggleActive);}
+	});
