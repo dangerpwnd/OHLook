bbc4c82a4f1c3d2858051e6c945daef3e3be4348
Eric Dangerstone <reric2012@gmail.com>
2019-07-19 12:38:04 -0500

    Added close button to modal

--- assets/JS/modal.js
+++ assets/JS/modal.js
@@ -1,5 +1,5 @@
 //variables
-
+const closeB = document.querySelectorAll(".closeButton");
 const modal1 = document.querySelector(".modal1");
 const modal2 = document.querySelector(".modal2");
 const mClick = document.querySelectorAll(".modalClick");
@@ -15,7 +15,21 @@ mClick.forEach(function(button){
             modal2.classList.toggle("showModal");
         }
         else{
-            console.log("Script not working");
+            console.log("Modal not working");
+        }
+});
+})
+
+closeB.forEach(function(close){
+    close.addEventListener("click", function toggleModal(){
+        if(modal1.classList.contains("showModal")){
+            modal1.classList.toggle("showModal");
+        }
+        else if(modal2.classList.contains("showModal")){
+            modal2.classList.toggle("showModal");
+        }
+        else{
+            console.log("Close button not working");
         }
 });
 })
