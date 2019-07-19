a6f9dc3c4c312c12a90bf7a8a03dfd9019cdcd96
Eric Dangerstone <reric2012@gmail.com>
2019-07-19 13:14:30 -0500

    Added modals to masterclass, private lessons

--- assets/JS/class.js
+++ assets/JS/class.js
@@ -1,18 +1,23 @@
-
+//variables
+const closeB = document.querySelectorAll(".closeButton");
+const modal1 = document.querySelector(".modal1");
+const modal2 = document.querySelector(".modal2");
+const modal3 = document.querySelector(".modal3");
+const mClick = document.querySelectorAll(".modalClick");
 const panels = document.querySelectorAll(".panel");
 const titles = document.querySelectorAll(".panelTitle");
 const links = document.querySelectorAll(".panelLink");
 var width = $(window).width();
 
-
+//Initial Tween Set for animated links
 TweenLite.set(links, {
 	xPercent: 100
 })
-
+//Color toggle animation for panel titles
 function toggleColor() {
 	this.classList.toggle('tColor');
 }
-
+//Panel navigation animation for mobile/PC
 if(width < 1025){
 	panels.forEach(function(panel){
 
@@ -53,3 +58,48 @@ else{
 		}
 	})
 }
+
+//Modal functionality
+
+// Event Listeners
+//Button Click toggle class
+mClick.forEach(function(button){
+    button.addEventListener("click", function toggleModal(){
+        if(this.classList.contains("mClick1")){
+            modal1.classList.toggle("showModal");
+        }
+        else if(this.classList.contains("mClick2")){
+            modal2.classList.toggle("showModal");
+		}
+		else if(this.classList.contains("mClick3")){
+            modal3.classList.toggle("showModal");
+        }
+        else{
+            console.log("Modal not working");
+        }
+});
+})
+//Close button toggle class
+closeB.forEach(function(close){
+    close.addEventListener("click", function toggleModal(){
+        if(modal1.classList.contains("showModal")){
+            modal1.classList.toggle("showModal");
+        }
+        else if(modal2.classList.contains("showModal")){
+            modal2.classList.toggle("showModal");
+		}
+		else if(modal3.classList.contains("showModal")){
+            modal3.classList.toggle("showModal");
+        }
+        else{
+            console.log("Close button not working");
+        }
+});
+})
+//Window click toggle class
+document.addEventListener("click", function toggleModal(event){
+    if(event.target.matches(".showModal")){
+        console.log(event.target);
+        (event.target).classList.remove('showModal');
+    }
+});
