2d45f214bbbe8262c102f8e0e2b9c5de0e3f371c
Eric Dangerstone <Eric@Dangerstone.com>
2019-07-20 11:30:03 -0500

    Updated modal close button

--- assets/JS/class.js
+++ assets/JS/class.js
@@ -20,11 +20,10 @@ function toggleColor() {
 //Panel navigation animation for mobile/PC
 if (width < 1025) {
     panels.forEach(function (panel) {
-
-        if (panel.classList.contains(".last")) {
             var timeLine = new TimelineMax();
             var tween = timeLine.add(TweenMax.to(panel, 0.5, { filter: "greyscale(0)" }));
             var tween2 = timeLine.add(TweenMax.to(panel.querySelectorAll(".panelLink"), 0.5, { xPercent: 0, autoAlpha: 1, visibility: "visible", ease: Power2.easeOut }))
+            console.log("last panel 1024 and below");
             panel.addEventListener("click", toggleAnimation);
             panel.addEventListener("click", toggleColor);
             timeLine.reverse();
@@ -37,30 +36,10 @@ if (width < 1025) {
                 }
             }
         }
-        else {
-
-            var timeLine = new TimelineMax();
-            var tween = timeLine.add(TweenMax.to(panel, 0.5, { flex: 2, filter: "greyscale(0)" }));
-            var tween2 = timeLine.add(TweenMax.to(panel.querySelectorAll(".panelLink"), 0.5, { xPercent: 0, autoAlpha: 1, visibility: "visible", ease: Power2.easeOut }))
-            panel.addEventListener("click", toggleAnimation);
-            panel.addEventListener("click", toggleColor);
-            timeLine.reverse();
-            function toggleAnimation() {
-                if (timeLine.reversed()) {
-                    timeLine.play();
-                }
-                else {
-                    timeLine.reverse();
-                }
-            }
-        }
-    })
+    )
 }
 else {
     panels.forEach(function (panel) {
-
-        if (panel.classList.contains(".last")) {
-
             var timeLine = new TimelineMax();
             var tween = timeLine.add(TweenMax.to(panel, 0.5, { filter: "greyscale(0)" }));
             var tween2 = timeLine.add(TweenMax.to(panel.querySelectorAll(".panelLink"), 0.5, { xPercent: 0, autoAlpha: 1, visibility: "visible", ease: Power2.easeOut }))
@@ -78,26 +57,7 @@ else {
                 }
             }
         }
-        else {
-            var timeLine = new TimelineMax();
-            var tween = timeLine.add(TweenMax.to(panel, 0.5, { flex: 3, filter: "greyscale(0)" }));
-            var tween2 = timeLine.add(TweenMax.to(panel.querySelectorAll(".panelLink"), 0.5, { xPercent: 0, autoAlpha: 1, visibility: "visible", ease: Power2.easeOut }))
-            panel.addEventListener("mouseenter", toggleAnimation);
-            panel.addEventListener("mouseenter", toggleColor);
-            panel.addEventListener("mouseleave", toggleAnimation);
-            panel.addEventListener("mouseleave", toggleColor);
-            timeLine.reverse();
-            function toggleAnimation() {
-                if (timeLine.reversed()) {
-                    timeLine.play();
-                }
-                else {
-                    timeLine.reverse();
-                }
-            }
-        }
-
-    })
+    )
 }
 
 //Modal functionality
