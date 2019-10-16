ad47514cfc750a879b782bec007889bf23b42938
Eric Dangerstone <eric@dangerstone.com>
2019-10-16 16:51:23 -0500

    Updated index with media queries

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,35 +1,13 @@
 //variables
 const panels = document.querySelectorAll(".indexPanel");
 const links = document.querySelectorAll(".indexLink");
-var width = $(window).width();
 
 //Color toggle animation for panel titles
 function toggleColor() {
 	this.classList.toggle('tColor');
 }
 //Panel navigation animation for mobile/PC
-if (width < 1025) {
-	panels.forEach(function (panel) {
-		var timeLine = new TimelineMax();
-		var tween = timeLine.add(TweenMax.to(panel, 0.5, { filter: "greyscale(0)" }));
-		panel.addEventListener("click", toggleAnimation);
-		timeLine.reverse();
-		function toggleAnimation() {
-			if (timeLine.reversed()) {
-				timeLine.play();
-			}
-			else {
-				timeLine.reverse();
-			}
-		}
-	}
-	)
-	links.forEach(function (link) {
-		link.addEventListener("click", toggleColor);
-	}
-	)
-}
-else {
+
 	panels.forEach(function (panel) {
 		var timeLine = new TimelineMax();
 		var tween = timeLine.add(TweenMax.to(panel, 0.5, { filter: "greyscale(0)" }));
@@ -51,8 +29,6 @@ else {
 		link.addEventListener("mouseleave", toggleColor);
 	}
 	)
-}
-
 
 
 
