e14cb730497757b2bc494161b3832d3a65d586c8
Eric Dangerstone <Eric@Dangerstone.com>
2020-01-03 18:27:19 -0600

    Finished animations

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -6,18 +6,14 @@ const $links = jQuery(".indexLink");
 
 //Color toggle animation for panel titles
 function toggleColor() {
-  jQuery(this).classList.toggle("tColor");
+  jQuery(this).toggleClass("tColor");
 }
+$links.hover(toggleColor, toggleColor);
 
-// //Active class toggle
-// function toggleActive() {
-// 	jQuery(this).toggleClass('Active');
-// }
 
 $panels.on("mouseenter", function(e) {
   var $thisPanel = jQuery(this);
-  var $action = new TimelineMax()
-    .to($thisPanel, 0.3, { x: 100, duration: 1 });
+  var $action = new TimelineMax().to($thisPanel, 0.3, { x: 100, duration: 1 });
   if ($thisPanel.hasClass("panel-front1")) {
     $action.set($frontinfo, {
       backgroundImage: "url(/assets/images/front1.jpg)"
@@ -36,7 +32,10 @@ $panels.on("mouseenter", function(e) {
     });
   } else if ($thisPanel.hasClass("panel-front5")) {
     $action.set($frontinfo, {
-      backgroundImage: "url(/assets/images/2020index.jpg)"
+      css: {
+        backgroundImage: "url(/assets/images/2020index.jpg)",
+        backgroundSize: "contain"
+      }
     });
   } else if ($thisPanel.hasClass("panel-front6")) {
     $action.set($frontinfo, {
@@ -53,71 +52,13 @@ $panels.on("mouseenter", function(e) {
   } else {
     return;
   }
-
-  }
-);
+});
 
 $panels.on("mouseleave", function(e) {
   var $thisPanel = $(this);
   var $action = new TimelineMax()
-  .to($thisPanel, 0.3, { x: 0, duration: 1 })
-  .set($frontinfo, {backgroundImage: "url(/assets/images/logo.png)"})
+    .to($thisPanel, 0.3, { x: 0, duration: 1 })
+    .set($frontinfo, { backgroundImage: "url(/assets/images/logo.png)" });
 });
 
-//Event Listeners
 
-// jQuery.each(panels, function(){
-// 	jQuery(this).hover(toggleActive, toggleActive);
-// })
-
-//Panel navigation animation for mobile/PC
-
-// 	jQuery.each(panels, function (panel) {
-// 		var timeLine = new TimelineMax();
-// 		var tween = timeLine.add(TweenMax.to(panel, 0.5, { x: 100, duration: 1 }));
-// 		if(jQuery(this).hasClass('panel-front1') && jQuery(this).hasClass('Active')){
-// 			let tween2 = timeLine.set(frontinfo, { backgroundImage:'url(/assets/images/front1.jpg)'});
-// 		}
-// 		else if(jQuery(this).hasClass('panel-front2') && jQuery(this).hasClass('Active')){
-// 			let tween2 = timeLine.set(frontinfo, { backgroundImage:'url(/assets/images/dance.jpg)'});
-// 		}
-// 		else if(jQuery(this).hasClass('panel-front3') && jQuery(this).hasClass('Active')){
-// 			let tween2 = timeLine.set(frontinfo, { backgroundImage:'url(/assets/images/front5.JPG)'});
-// 		}
-// 		else if(jQuery(this).hasClass('panel-front4') && jQuery(this).hasClass('Active')){
-// 			let tween2 = timeLine.set(frontinfo, { backgroundImage:'url(/assets/images/front8.jpg)'});
-// 		}
-// 		else if(jQuery(this).hasClass('panel-front5') && jQuery(this).hasClass('Active')){
-// 			let tween2 = timeLine.set(frontinfo, { backgroundImage:'url(/assets/images/2020index.jpg)'});
-// 		}
-// 		else if(jQuery(this).hasClass('panel-front6') && jQuery(this).hasClass('Active')){
-// 			let tween2 = timeLine.set(frontinfo, { backgroundImage:'url(/assets/images/front4.JPG)'});
-// 		}
-// 		else if(jQuery(this).hasClass('panel-front7') && jQuery(this).hasClass('Active')){
-// 			let tween2 = timeLine.set(frontinfo, { backgroundImage:'url(/assets/images/front6.JPG)'});
-// 		}
-// 		else if(jQuery(this).hasClass('panel-front8') && jQuery(this).hasClass('Active')){
-// 			let tween2 = timeLine.set(frontinfo, { backgroundImage:'url(/assets/images/xanadu.jpg)'});
-// 		}
-// 		else{
-// 			return;
-// 		}
-// 		jQuery(this).hover(toggleAnimation, toggleAnimation);
-// 		timeLine.reverse();
-// 		function toggleAnimation() {
-// 			if (timeLine.reversed()) {
-// 				timeLine.play();
-// 			}
-// 			else {
-// 				timeLine.reverse();
-// 			}
-// 		}
-// 	}
-// 	);
-
-// // Color Toggle Animation
-// 	links.forEach(function (link) {
-// 		link.addEventListener("mouseenter", toggleColor);
-// 		link.addEventListener("mouseleave", toggleColor);
-// 	}
-// 	);
